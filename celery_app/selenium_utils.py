ea4019df29b0df9c69b2c448f494ee60167759be
Ivan Suprun <ivan.suprun2015@gmail.com>
2023-08-09 08:44:27 +0300

    feat(parsing): added parsing of site
    Added parsing from "https://www.bbc.com/news/world/africa" every hour. Using selenium, the link is opened, all posts are received and the data is parsed using a function from selenium utils. Selenium utils has the following functions: get_header, get_post_url, get_post_data, get_img, get_time and get_preview.

---
+++ celery_app/selenium_utils.py
@@ -0,0 +1,126 @@
+import os
+from datetime import datetime
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+class GetOutOfLoop(Exception):
+    pass
+
+
+def get_header(article: WebElement) -> str:
+
+    header = article.find_element(By.TAG_NAME, 'header')
+    header_span = header.find_element(By.TAG_NAME, 'span')
+
+    return header_span.text
+
+
+def get_post_url(article: WebElement) -> str:
+
+    header = article.find_element(By.TAG_NAME, 'header')
+    url = header.find_element(By.TAG_NAME, "a").get_attribute("href")
+
+    return url
+
+
+def get_post_data(url: str):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    post_driver = webdriver.Chrome(options=chrome_options)
+    post_driver.get(url)
+
+    post_article = post_driver.find_element(By.TAG_NAME, 'article')
+
+    # get post header
+    header = post_article.find_element(By.TAG_NAME, 'header')
+    h1 = header.find_element(By.TAG_NAME, 'h1')
+
+    # get post body
+    body = post_article.find_elements(By.XPATH,
+                                      "//div[@data-component!='links-block' and @data-component!='topic-list' and @data-component!='regionalIndexExternalLinks-disclaimer' and not(h2)]")
+    body = ["\n"+e.get_attribute('outerHTML') for e in body]
+    body = ' '.join(body)
+    
+    return h1.text, body
+
+
+def get_img(div: WebElement, is_preview: bool, article: WebElement) -> str:
+    img_div = div
+
+    img = img_div.find_element(By.TAG_NAME, 'img')
+
+    url = img.get_attribute('src')
+
+    if is_preview:
+        url = img.get_attribute('data-src')
+        url = url.replace('{width}', '624')
+    else:
+        url = url.replace("/320/", "/800/")
+
+    # print(url)
+    response = requests.get(url)
+
+    header = article.find_element(By.TAG_NAME, 'header')
+    img_name = ''.join(e for e in header.text if e.isalnum())
+
+    with open(os.path.join(os.path.realpath('.')+"\\frontend\\public\\uploads", f"{img_name.lower()}.png"), "wb") as f:
+        f.write(response.content)
+    return img_name.lower()+'.png'
+
+
+def get_time(article: WebElement) -> datetime:
+    time_div = article.find_element(By.TAG_NAME, 'div')
+    time_div = time_div.find_element(By.TAG_NAME, 'time')
+    time_spans = time_div.find_elements(By.TAG_NAME, 'span')
+
+    datetime_str = time_spans[1].text
+    datetime_str = datetime_str.replace(" ", "")
+
+    try:
+        year = int(datetime_str[-4:])
+
+        # if no exception return date
+        month_str = datetime_str[6:9]
+        month_num = datetime.strptime(month_str, '%b').month
+
+        datetime_str = datetime_str.replace(month_str, str(month_num))
+        datetime_odj = datetime.strptime(datetime_str, "%H:%M%d%m%Y")
+
+        return datetime_odj
+    except ValueError:
+        pass
+
+    try:
+        month = datetime.strptime(datetime_str[-3:], '%b').month
+
+        # if no exception return date
+        datetime_str = datetime_str.replace(datetime_str[-3:], str(month))
+        datetime_str = datetime_str + str(datetime.now().year)
+        datetime_odj = datetime.strptime(datetime_str, "%H:%M%d%m%Y")
+        return datetime_odj
+    except ValueError:
+        pass
+
+    datetime_str = datetime_str + str(datetime.now().day)
+    datetime_str = datetime_str + str(datetime.now().month)
+    datetime_str = datetime_str + str(datetime.now().year)
+    datetime_odj = datetime.strptime(datetime_str, "%H:%M%d%m%Y")
+
+    return datetime_odj
+
+
+def get_preview(preview) -> str:
+    if preview is WebElement:
+        preview_text = preview.find_element(By.TAG_NAME, 'p')
+        return preview_text.text
+    else:
+        preview_texts = preview.find_elements(By.TAG_NAME, 'p')
+        preview_text = ["\n"+e.text for e in preview_texts]
+        preview_text = ' '.join(preview_text)
+    return preview_text
--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -1,11 +1,119 @@
-from app import celery_app
+import time
+
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+from backend import celery_app, db
+from backend.models import NewsPreview, News
+from celery_app.selenium_utils import get_header, get_post_url, get_preview, get_time, get_img, get_post_data, \
+    GetOutOfLoop
 
 
 @celery_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(5.0, test.s('hello world'), name='add every 5')
+    sender.add_periodic_task(1.0*60*60, parsing.s(), name='get posts')
 
 
 @celery_app.task
-def test(arg):
-    print(arg)
+def parsing():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.maximize_window()
+
+    driver.get("https://www.bbc.com/news/world/africa")
+
+    lats_parsed = NewsPreview.query.filter_by(is_parsed=True).order_by(NewsPreview.posted_at.desc()).first()
+
+    try:
+        while True:
+            elements = driver.find_elements(By.CLASS_NAME, 'lx-stream__post-container')
+
+            for element in elements:
+                print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+                is_preview = False
+
+                article = element.find_element(By.XPATH, '*')
+
+                # get header
+                header = get_header(article)
+                print("Preview header: "+header)
+
+                # get post
+                try:
+                    url = get_post_url(article)
+
+                    if 'https://www.bbc.com/news/' in url:
+                        post_header, body = get_post_data(url)
+                        # print("Post header: " + post_header)
+                        # print(body)
+                    else:
+                        # print("---------------Body of post not BBC site!---------------")
+                        continue
+                except NoSuchElementException:
+                    is_preview = True
+                    # print("--------------------Post only preview!--------------------")
+
+                # get body of preview
+                body_div = article.find_element(By.CSS_SELECTOR, 'div.gs-u-mb\+.gel-body-copy.qa-post-body')
+                body_div = body_div.find_element(By.TAG_NAME, 'div')
+                body_divs = body_div.find_elements(By.TAG_NAME, 'div')
+
+                try:
+                    body_div.find_element(By.TAG_NAME, 'img')
+                except NoSuchElementException:
+                    # print("---------------Post without img!---------------")
+                    continue
+
+                # get img
+                img = get_img(body_divs[0], is_preview, article)
+                # print("Image: "+img)
+
+                # get time
+                datetime_odj = get_time(article)
+                date = datetime_odj.strftime("%Y-%m-%dT%H:%M:%S")
+                # print("Date: "+str(date))
+
+                # get preview
+                if not is_preview:
+                    preview = body_divs[2]
+                else:
+                    preview = body_div
+
+                preview = get_preview(preview)
+                # print("Preview:")
+                # print(preview)
+
+                if header == lats_parsed.title:
+                    raise GetOutOfLoop
+                else:
+                    preview = NewsPreview(
+                        img=img,
+                        title=header,
+                        posted_at=date,
+                        preview=preview,
+                        user_id=None,
+                        is_parsed=True
+                    )
+
+                    db.session.add(preview)
+                    db.session.flush()
+                    db.session.refresh(preview)
+
+                    if not is_preview:
+                        post = News(title=post_header,
+                                    text=body,
+                                    preview_id=preview.id)
+                        db.session.add(post)
+
+                    db.session.commit()
+
+            btn = driver.find_element(By.XPATH, "//a[@rel='next']")
+
+            btn.click()
+            time.sleep(2)
+    except GetOutOfLoop:
+        pass
