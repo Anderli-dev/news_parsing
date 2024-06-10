a05a2061ce82196383e9272067bc477cc1abfeed
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2024-06-10 21:14:37 +0300

    refactor(parsing): refactored parsing acording to new style of site

--- parsing_app/parsing.py
+++ parsing_app/parsing.py
@@ -15,7 +15,7 @@ def parsing():
     with app.app_context():
         # Selenium settings
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless=new")
 
         driver = webdriver.Chrome(options=chrome_options)
         driver.maximize_window()
@@ -24,65 +24,59 @@ def parsing():
 
         lats_parsed = NewsPreview.query.filter_by(is_parsed=True).order_by(NewsPreview.posted_at.desc()).first()
 
+        pag_page = 1
         # Parsing logic
         try:
             while True:
-                elements = driver.find_elements(By.CLASS_NAME, 'lx-stream__post-container')
-
+                elements = driver.find_elements(By.XPATH, ".//div[@data-testid='liverpool-card']")
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 for element in elements:
+
                     print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
                     is_preview = False
 
-                    article = element.find_element(By.XPATH, '*')
-
                     # get header
-                    header = get_header(article)
+                    header = get_header(element)
                     print("Preview header: "+header)
 
                     # get post
                     try:
-                        url = get_post_url(article)
+                        url = get_post_url(element)
+                        print("URL:", url)
 
-                        if 'https://www.bbc.com/news/' in url:
+                        if 'https://www.bbc.com/news/articles/' in url:
                             post_header, body = get_post_data(url)
-                            # print("Post header: " + post_header)
-                            # print(body)
+                            print("Post header: " + post_header)
+                            print("Post body:")
+                            print(body)
                         else:
-                            # print("---------------Body of post not BBC site!---------------")
+                            print("---------------Body of post not BBC site!---------------")
                             continue
                     except NoSuchElementException:
                         is_preview = True
-                        # print("--------------------Post only preview!--------------------")
-
-                    # get body of preview
-                    body_div = article.find_element(By.CSS_SELECTOR, 'div.gs-u-mb\+.gel-body-copy.qa-post-body')
-                    body_div = body_div.find_element(By.TAG_NAME, 'div')
-                    body_divs = body_div.find_elements(By.TAG_NAME, 'div')
+                        print("--------------------Post only preview!--------------------")
 
                     # get img
                     try:
-                        body_div.find_element(By.TAG_NAME, 'img')
+                        element.find_element(By.XPATH, "//div[@data-testid='card-media']")
                     except NoSuchElementException:
-                        # print("---------------Post without img!---------------")
+                        print("---------------Post without img!---------------")
                         continue
 
-                    img = get_img(body_divs[0], is_preview, article)
-                    # print("Image: "+img)
+                    img = get_img(element.find_element(By.XPATH, ".//div[@data-testid='card-media']"), is_preview, element)
+                    print("Image: "+img)
 
                     # get time
-                    datetime_odj = get_time(article)
+                    datetime_odj = get_time(element)
                     date = datetime_odj.strftime("%Y-%m-%dT%H:%M:%S")
-                    # print("Date: "+str(date))
+                    print("Date: "+str(date))
 
                     # get preview
-                    if not is_preview:
-                        preview = body_divs[2]
-                    else:
-                        preview = body_div
+                    # Currently BBC don`t use preview posts
 
-                    preview = get_preview(preview)
-                    # print("Preview:")
-                    # print(preview)
+                    preview = get_preview(element)
+                    print("Preview:")
+                    print(preview)
 
                     print(lats_parsed)
 
@@ -100,27 +94,31 @@ def parsing():
                             is_parsed=True
                         )
 
-                        db.session.add(preview)
-                        db.session.flush()
-                        db.session.refresh(preview)
-
-                        if not is_preview:
-                            try:
-                                post = News(title=post_header,
-                                            text=body,
-                                            preview_id=None)
-                            except:
-                                db.session.rollback()
-                                continue
-                            else:
-                                post.preview_id = preview.id
-                                db.session.add(post)
-
-                        db.session.commit()
+                        # db.session.add(preview)
+                        # db.session.flush()
+                        # db.session.refresh(preview)
+                        #
+                        # if not is_preview:
+                        #     try:
+                        #         post = News(title=post_header,
+                        #                     text=body,
+                        #                     preview_id=None)
+                        #     except:
+                        #         db.session.rollback()
+                        #         continue
+                        #     else:
+                        #         post.preview_id = preview.id
+                        #         db.session.add(post)
+                        #
+                        # db.session.commit()
 
                 try:
-                    btn = driver.find_element(By.XPATH, "//a[@rel='next']")
-                    btn.click()
+                    btn = driver.find_element(By.XPATH, ".//button[@data-testid='pagination-next-button']")
+                    if pag_page < 11:
+                        btn.click()
+                        pag_page = pag_page+1
+                    else:
+                        raise GetOutOfLoop
                 except NoSuchElementException:
                     raise GetOutOfLoop
 
