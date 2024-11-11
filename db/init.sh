1cd5aa32e7a23bee2c7f5ab72cf2a6fbb33d029a
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2024-11-11 08:14:42 +0200

    build(docker): added script for initializing db
    Added sh script where inserting base data to permission, role, role_permission columns.

---
+++ db/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MYSQL_USER="admin"
+MYSQL_PASSWORD="12345"
+MYSQL_HOST="localhost"
+MYSQL_DATABASE="mydb"
+
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < "$(dirname "$0")/init_roles.sql"
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < "$(dirname "$0")/init_perms.sql"
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < "$(dirname "$0")/init_r_p.sql"
+
